379f90771bd73e08d721bc3bc26203300b9cdc14
esukhova <e_ukhova@inbox.ru>
2025-02-11 21:13:01 +0300

    fixed http replacement in catalog comp

--- src/app/views/products/catalog/catalog.component.ts
+++ src/app/views/products/catalog/catalog.component.ts
@@ -56,10 +56,6 @@ export class CatalogComponent implements OnInit, OnDestroy {
               this.products = products;
               this.status = 2;
               this.inputValue = value;
-
-              this.products.forEach((product) => {
-                product.image = product.image.replace('http', 'https');
-              })
             } else {
               this.products = [];
               this.status = 0;
@@ -72,6 +68,10 @@ export class CatalogComponent implements OnInit, OnDestroy {
               this.status = 0;
             }
           }
+
+          this.products.forEach((product) => {
+            product.image = product.image.replace('http', 'https');
+          })
         },
         error: (error) => {
           console.log(error);
